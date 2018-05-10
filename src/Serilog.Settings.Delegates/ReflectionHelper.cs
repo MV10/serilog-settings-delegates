4cf7d9e00f18c40e76d31ce1172a84a5695fc9fb
Jon McGuire <mcguirev10@gmail.com>
2018-05-10 17:53:33 -0400

    exclude testhost assembly

--- src/Serilog.Settings.Delegates/ReflectionHelper.cs
+++ src/Serilog.Settings.Delegates/ReflectionHelper.cs
@@ -47,18 +47,19 @@ namespace Serilog.Settings.Delegates
             // message "Can't create a metadata reference to a dynamic assembly" running xUnit
             // tests in Visual Studio or Windows.
 
-            // WindowsBase?
+            // testhost?
             // Exclude ??? assemblies which throws a System.Reflection.ReflectionTypeLoadException
             // with the message "Unable to load one or more of the requested types" when reading
             // xunit.core namespaces during unit testing on Linux in the Travis-CI process.
 
             // Exclude testing-specific assemblies (xUnit, Microsoft.VisualStudio.*, and
-            // Microsoft.TestPlatform)
+            // Microsoft.TestPlatform) and Linux Travis-CI assemblies (WindowsBase, 
 
             return
                 assemblies
                 .Where(n =>
                     !n.IsDynamic
+                    && !n.FullName.StartsWith("testhost", StringComparison.InvariantCultureIgnoreCase)
                     && !n.FullName.StartsWith("WindowsBase", StringComparison.InvariantCultureIgnoreCase)
                     && !n.FullName.StartsWith("xunit", StringComparison.InvariantCultureIgnoreCase))
                 .ToList();
